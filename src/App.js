f6959be7b166f4d0e44ca114b1e5e2502ac15af8
CancuiW <can.cui@wayne.edu>
2023-07-07 11:35:13 -0700

    challenge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from "styled-components";
+const StyledItems=styled.div`
+  display: grid;
+  place-items: center;
+
+`
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +17,32 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data,setData]=useState([])
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then(res => {
+        setData(res.data)
+        //console.log(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledItems>
+        {data.map((x,index)=>{
+         return <Character info={x} key={index} />
+        })
+      }
+
+      </StyledItems>
+        
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter=styled.div`
+     width:80%;
+     display:flex;
+     justify-content: space-between;
+     flex-direction:row;
+     border:2px solid green;
+     margin:8px ; 
+    
+     
+`
+const StyledName=styled.p`
+     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+     font-weight: 800;
+      font-size: larger;
+      margin-left:8px;
+`
+const StyledYear=styled.p`
+        border:2px solid grey;
+        background-color:grey;
+        padding:10px ; 
+        margin-right:8px;
+        color:purple;
+        border-radius: 10px;
+
+`
+
+const Character=props=>{
+    return (
+        <StyledCharacter>
+            <StyledName>{props.info.name}</StyledName>
+            <StyledYear>{props.info.birth_year}</StyledYear>
+        
+        </StyledCharacter>
+
+    )
+}
+export default Character;
